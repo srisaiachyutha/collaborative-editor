b3a21a3ab0c8a5ce5f13f76113da42828af6abe6
punna sri sai achyutha <44301170+srisaiachyutha@users.noreply.github.com>
2024-01-03 04:49:07 +0000

    update base in vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/collaborative-editor/",
+  // base:"/collaborative-editor/",
+  base: "./",
   plugins: [react()],
   test: {
     globals: true,
